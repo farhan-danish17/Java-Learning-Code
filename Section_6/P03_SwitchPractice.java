bfa4710e35926915e74eca1751b307fae3a35cf6
Farhan Danish <farhancodes17@gmail.com>
2025-06-26 16:22:16 +0530

    Question putting

--- Section_6/P03_SwitchPractice.java
+++ Section_6/P03_SwitchPractice.java
@@ -1,3 +1,18 @@
+/*
+In this challenge, we'll be using the NATO alphabet to replace a character or letter, with NATO's standardized word for that letter.
+In radio transmissions, the word car, "C", "A", "R", would be read, "Charlie Able Roger", for clarity.
+We'll take a single character, and return the matching word from the NATO phonetic alphabet, shown on this slide.
+We'll just do this for the letters A, through E.
+
+To do this:
+Create a new char variable.
+Use the traditional switch statement (with a colon in case labels) that tests the value in the variable from Step 1. 
+Create cases for the characters,  A, B, C, D, and E.
+
+Display a message in each case block, with the letter and the NATO word, then break.
+Add a default block, which displays the letter with a message saying not found.
+*/
+
 package Section_6;
 
 public class P03_SwitchPractice {
---
+++ Section_6/P04_EmbracingSwitchPractice.java
@@ -0,0 +1,7 @@
+package Section_6;
+
+public class P04_EmbracingSwitchPractice {
+    public static void main(String[] args) {
+        
+    }
+}
