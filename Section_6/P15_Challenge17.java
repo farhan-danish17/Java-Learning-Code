fdb4fdb049d81593837d2b4d11f8160fe363e69d
Farhan Danish <farhancodes17@gmail.com>
2025-06-27 14:11:39 +0530

    Challenge17

--- Section_6/P15_Challenge17.java
+++ Section_6/P15_Challenge17.java
@@ -11,5 +11,19 @@ If the number is negative then the method needs to return -1 to indicate an inva
 package Section_6;
 
 public class P15_Challenge17 {
-    
+    public class FirstLastDigitSum {
+    // write code here
+    public static int sumFirstAndLastDigit (int number) {
+        if (number < 0) {
+            return -1;
+        }
+        
+        int lastDigit = number % 10;
+          int firstDigit = number;
+          while (firstDigit >= 10) {
+               firstDigit /= 10;
+          }
+          return firstDigit + lastDigit;
+    }
+}
 }
