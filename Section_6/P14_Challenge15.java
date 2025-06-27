0b7e63dabb4b9d3f0b5154bc260922a3f3a4a08f
Farhan Danish <farhancodes17@gmail.com>
2025-06-27 14:00:49 +0530

    Challenge15

---
+++ Section_6/P14_Challenge15.java
@@ -0,0 +1,32 @@
+/*
+ Identifying Palindromic Numbers For Enhanced Numeric Validation
+Write a method called isPalindrome with one int parameter called number.
+
+The method needs to return a boolean.
+
+It should return true if the number is a palindrome number otherwise it should return false.
+
+
+*/
+
+
+package Section_6;
+
+public class P14_Challenge15 {
+    public class NumberPalindrome {
+    // write code here
+    public static boolean isPalindrome (int number) {
+        
+        int originalNumber = number;
+        int reversed = 0;
+        number = Math.abs(number);
+        
+        while(number != 0 || number > 0) {
+            int lastDigit = number % 10;
+               reversed = reversed * 10 + lastDigit;
+               number /= 10;
+        }
+        return Math.abs(originalNumber) == reversed;
+    }
+}
+}
