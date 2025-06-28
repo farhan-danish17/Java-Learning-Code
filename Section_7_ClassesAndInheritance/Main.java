3ecc3aa28736fba5713950f658593d031dca9fea
Farhan Danish <farhancodes17@gmail.com>
2025-06-28 12:41:56 +0530

    Setters, Object Creation

--- Section_7_ClassesAndInheritance/Main.java
+++ Section_7_ClassesAndInheritance/Main.java
@@ -3,11 +3,22 @@ package Section_7_ClassesAndInheritance;
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
-        // car.make() = "Porsche";
-        // car.model() = "Carrera";
-        // car.colour() = "Red";
+        car.setMake("Porsche");
+        car.setModel("Carrera");
+        car.setColour("Black");
+        car.setDoors(2);
+        car.setConvertible(true);
         System.out.println("make = " + car.getMake());
         System.out.println("model = " + car.getModel());
         car.describeCar();
+
+        Car targa = new Car();
+        targa.setMake("Porsche");
+        targa.setModel("Targa");
+        targa.setColour("red");
+        targa.setDoors(2);
+        targa.setConvertible(false);
+
+        targa.describeCar();
     }
 }
